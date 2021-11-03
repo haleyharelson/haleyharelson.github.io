ce47a93b473700cdb97626e6a29fd8086b81c0e8
Haley Harelson <hharelso@asu.edu>
2021-11-03 16:04:08 -0700

    created geo maps

--- js/main.js
+++ js/main.js
@@ -21,11 +21,6 @@ let yScaleRaceChart;
 let xAxisRaceChart;
 let yAxisRaceChart;
 
-// for geo map
-let marginUCLAMap;
-let widthUCLAMap;
-let heightUCLAMap;
-
 // This function is called once the HTML page is fully loaded by the browser
 document.addEventListener('DOMContentLoaded', function () {
     drawGenderPercentageBarChart();
@@ -34,7 +29,8 @@ document.addEventListener('DOMContentLoaded', function () {
     drawGenderLollipopChart('AvgUCLAScore');
     drawRaceLollipopCanvas();
     drawRaceLollipopChart('AvgUCLAScore');
-    // drawMap();
+    drawUCLAMap();
+    drawPROMISMap();
     drawBarChart();
 });
 
@@ -399,11 +395,7 @@ function drawRaceLollipopChart(selectedScore) {
     });
 }
 
-function drawMap() {
-    count = 0;
-    count++;
-    console.log("function called")
-    console.log("count 1", count)
+function drawUCLAMap() {
     //Width and height of map
     var margin = {top: 40, bottom: 90, right: 20, left: 190};
     var width = 1300 - margin.left - margin.right;
@@ -412,114 +404,236 @@ function drawMap() {
     var lowColor = '#f9f9f9'
     var highColor = '#bc2a66'
 
-    console.log(width, height);
-
     // D3 Projection
     var projection = d3.geoAlbersUsa()
-        .translate([width / 2, height / 2]) // translate to center of screen
-        .scale([1000]); // scale things down so see entire US
+    .translate([width / 2, height / 2]) // translate to center of screen
+    .scale([1000]); // scale things down so see entire US
 
     // Define path generator
     var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
-        .projection(projection); // tell path generator to use albersUsa projection
+    .projection(projection); // tell path generator to use albersUsa projection
 
     //Create SVG element and append map to the SVG
-    var svg = d3.select('#mapSvg')
-        .attr("width", width)
-        .attr("height", height);
+    var svg = d3.select('#uclaMapSvg')
+    .attr("width", width)
+    .attr("height", height);
 
-        // Load in states data
-    d3.csv("States Score.csv", function(data) {
-        console.log("entered function")
+    d3.csv('/data/States UCLA Score.csv')
+    .then(function(data) {
         var dataArray = [];
         for (var d = 0; d < data.length; d++) {
-            console.log(d)
-            console.log("value:", data[d].value)
             dataArray.push(parseFloat(data[d].value))
         }
-        //console.log("length:", dataArray.length)
+        console.log("length:", dataArray.length)
         var minVal = d3.min(dataArray)
         var maxVal = d3.max(dataArray)
         var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor])
-        //console.log("CHECK")
-        // Load GeoJSON data and merge with states data
-        d3.json("us-states.json", function(json) {
-            console.log("TEST")
-            // Loop through each state data value in the .csv file
-            for (var i = 0; i < data.length; i++) {
-            // Grab State Name
-            var dataState = data[i].state;
-            console.log("state name:", dataState)
-            // Grab data value 
-            var dataValue = data[i].value;
-            // Find the corresponding state inside the GeoJSON
-            for (var j = 0; j < json.features.length; j++) {
-                var jsonState = json.features[j].properties.name;
-                if (dataState == jsonState) {
-                // Copy the data value into the JSON
-                json.features[j].properties.value = dataValue;
-                // Stop looking through the JSON
-                break;
-                }
+	
+    // Load GeoJSON data and merge with states data
+    d3.json("/data/us-states.json")
+    .then(function(json) {
+        // Loop through each state data value in the .csv file
+        for (var i = 0; i < data.length; i++) {
+        // Grab State Name
+        var dataState = data[i].state;
+        // Grab data value 
+        var dataValue = data[i].value;
+        // Find the corresponding state inside the GeoJSON
+        for (var j = 0; j < json.features.length; j++) {
+            var jsonState = json.features[j].properties.name;
+            if (dataState == jsonState) {
+            // Copy the data value into the JSON
+            json.features[j].properties.value = dataValue;
+            // Stop looking through the JSON
+            break;
             }
         }
+        }
 
-            // Bind the data to the SVG and create one path per GeoJSON feature
-            svg.selectAll("path")
-            .data(json.features)
-            .enter()
-            .append("path")
-            .attr("d", path)
-            .style("stroke", "#fff")
-            .style("stroke-width", "1")
-            .style("fill", function(d) { return ramp(d.properties.value) });
-            
-                // add a legend
-                var w = 140, h = 300;
+    // Bind the data to the SVG and create one path per GeoJSON feature
+    svg.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", function(d) { return ramp(d.properties.value) });
+    
+    svg.append("text")
+      .attr("x", (width / 2) - 20)             
+      .attr("y", 60 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "24px") 
+      .style("text-decoration", "underline")  
+      .text("Average UCLA Score by US State");
+    
+		// add a legend
+		var w = 140, h = 300;
 
-                var key = d3.select('#mapSvg')
-                    .attr("width", w)
-                    .attr("height", h)
-                    .attr("class", "legend");
+		var key = d3.select('#uclaMapSvg')
+			.attr("width", w)
+			.attr("height", h)
+			.attr("class", "legend");
 
-                var legend = key.append("defs")
-                    .append("svg:linearGradient")
-                    .attr("id", "gradient")
-                    .attr("x1", "100%")
-                    .attr("y1", "0%")
-                    .attr("x2", "100%")
-                    .attr("y2", "100%")
-                    .attr("spreadMethod", "pad");
+		var legend = key.append("defs")
+			.append("svg:linearGradient")
+			.attr("id", "gradient")
+			.attr("x1", "100%")
+			.attr("y1", "0%")
+			.attr("x2", "100%")
+			.attr("y2", "100%")
+			.attr("spreadMethod", "pad");
 
-                legend.append("stop")
-                    .attr("offset", "0%")
-                    .attr("stop-color", highColor)
-                    .attr("stop-opacity", 1);
-                    
-                legend.append("stop")
-                    .attr("offset", "100%")
-                    .attr("stop-color", lowColor)
-                    .attr("stop-opacity", 1);
+		legend.append("stop")
+			.attr("offset", "0%")
+			.attr("stop-color", highColor)
+			.attr("stop-opacity", 1);
+			
+		legend.append("stop")
+			.attr("offset", "100%")
+			.attr("stop-color", lowColor)
+			.attr("stop-opacity", 1);
 
-                key.append("rect")
-                    .attr("width", w - 100)
-                    .attr("height", h)
-                    .style("fill", "url(#gradient)")
-                    .attr("transform", "translate(0,10)");
+		key.append("rect")
+			.attr("width", w - 100)
+			.attr("height", h)
+			.style("fill", "url(#gradient)")
+			.attr("transform", "translate(0,10)");
 
-                var y = d3.scaleLinear()
-                    .range([h, 0])
-                    .domain([minVal, maxVal]);
+		var y = d3.scaleLinear()
+			.range([h, 0])
+			.domain([minVal, maxVal]);
 
-                var yAxis = d3.axisRight(y);
+		var yAxis = d3.axisRight(y);
 
-                key.append("g")
-                    .attr("class", "y axis")
-                    .attr("transform", "translate(41,10)")
-                    .call(yAxis)
-            });
+		key.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(41,10)")
+			.call(yAxis)	
         });
-    }
+    });
+}
+
+function drawPROMISMap() {
+    //Width and height of map
+    var margin = {top: 40, bottom: 90, right: 20, left: 190};
+    var width = 1300 - margin.left - margin.right;
+    var height = 700 - margin.top - margin.bottom;
+
+    var lowColor = '#f9f9f9'
+    var highColor = '#bc2a66'
+
+    // D3 Projection
+    var projection = d3.geoAlbersUsa()
+    .translate([width / 2, height / 2]) // translate to center of screen
+    .scale([1000]); // scale things down so see entire US
+
+    // Define path generator
+    var path = d3.geoPath() // path generator that will convert GeoJSON to SVG paths
+    .projection(projection); // tell path generator to use albersUsa projection
+
+    //Create SVG element and append map to the SVG
+    var svg = d3.select('#promisMapSvg')
+    .attr("width", width)
+    .attr("height", height);
+
+    d3.csv('/data/States PROMIS Score.csv')
+    .then(function(data) {
+        var dataArray = [];
+        for (var d = 0; d < data.length; d++) {
+            dataArray.push(parseFloat(data[d].value))
+        }
+        console.log("length:", dataArray.length)
+        var minVal = d3.min(dataArray)
+        var maxVal = d3.max(dataArray)
+        var ramp = d3.scaleLinear().domain([minVal,maxVal]).range([lowColor,highColor])
+	
+    // Load GeoJSON data and merge with states data
+    d3.json("/data/us-states.json")
+    .then(function(json) {
+        // Loop through each state data value in the .csv file
+        for (var i = 0; i < data.length; i++) {
+        // Grab State Name
+        var dataState = data[i].state;
+        // Grab data value 
+        var dataValue = data[i].value;
+        // Find the corresponding state inside the GeoJSON
+        for (var j = 0; j < json.features.length; j++) {
+            var jsonState = json.features[j].properties.name;
+            if (dataState == jsonState) {
+            // Copy the data value into the JSON
+            json.features[j].properties.value = dataValue;
+            // Stop looking through the JSON
+            break;
+            }
+        }
+        }
+
+    // Bind the data to the SVG and create one path per GeoJSON feature
+    svg.selectAll("path")
+      .data(json.features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .style("stroke", "#fff")
+      .style("stroke-width", "1")
+      .style("fill", function(d) { return ramp(d.properties.value) });
+    
+    svg.append("text")
+      .attr("x", (width / 2) - 20)             
+      .attr("y", 60 - (margin.top / 2))
+      .attr("text-anchor", "middle")  
+      .style("font-size", "24px") 
+      .style("text-decoration", "underline")  
+      .text("Average PROMIS Score by US State");
+    
+		// add a legend
+		var w = 140, h = 300;
+
+		var key = d3.select('#promisMapSvg')
+			.attr("width", w)
+			.attr("height", h)
+			.attr("class", "legend");
+
+		var legend = key.append("defs")
+			.append("svg:linearGradient")
+			.attr("id", "gradient")
+			.attr("x1", "100%")
+			.attr("y1", "0%")
+			.attr("x2", "100%")
+			.attr("y2", "100%")
+			.attr("spreadMethod", "pad");
+
+		legend.append("stop")
+			.attr("offset", "0%")
+			.attr("stop-color", highColor)
+			.attr("stop-opacity", 1);
+			
+		legend.append("stop")
+			.attr("offset", "100%")
+			.attr("stop-color", lowColor)
+			.attr("stop-opacity", 1);
+
+		key.append("rect")
+			.attr("width", w - 100)
+			.attr("height", h)
+			.style("fill", "url(#gradient)")
+			.attr("transform", "translate(0,10)");
+
+		var y = d3.scaleLinear()
+			.range([h, 0])
+			.domain([minVal, maxVal]);
+
+		var yAxis = d3.axisRight(y);
+
+		key.append("g")
+			.attr("class", "y axis")
+			.attr("transform", "translate(41,10)")
+			.call(yAxis)	
+        });
+    });
+}
 
 function drawBarChart() {
     // Draw canvas for barchart 
